dc8f0e5a9753e330cc35bba57c452be1c81a729d
Dan Mace <ironcladlou@gmail.com>
2018-02-20 15:49:39 -0500

    Add self-monitoring collectors
    Set up some default ServiceMonitors for self-monitoring.

--- roles/openshift_prometheus_operator/deploy.sh
+++ roles/openshift_prometheus_operator/deploy.sh
@@ -12,11 +12,7 @@ printf "Waiting for Operator to register custom resource definitions..."
 until oc get customresourcedefinitions servicemonitors.monitoring.coreos.com > /dev/null 2>&1; do sleep 1; printf "."; done
 echo "Done!"
 
-oc apply -f files/manifests/prometheus/prometheus-k8s-service-account.yaml
-oc apply -f files/manifests/prometheus/prometheus-k8s-roles.yaml
-oc apply -f files/manifests/prometheus/prometheus-k8s-role-bindings.yaml
-oc apply -f files/manifests/prometheus/prometheus-k8s.yaml
-oc apply -f files/manifests/prometheus/prometheus-k8s-service.yaml
+oc apply -f files/manifests/prometheus
 
 oc expose service prometheus-k8s
 
