6650f19a16998126b56928c380a2252455e6dd6f
Elad Alfassa <ealfassa@redhat.com>
2018-02-20 15:15:13 +0200

    Add cluster-reader permission to the right serviceaccount
    instead of default

--- roles/openshift_prometheus_operator/deploy.sh
+++ roles/openshift_prometheus_operator/deploy.sh
@@ -6,7 +6,7 @@ oc new-project monitoring
 
 oc apply -f files/manifests/prometheus-operator
 
-oc adm policy add-cluster-role-to-user cluster-reader -z default
+oc adm policy add-cluster-role-to-user cluster-reader -z prometheus-operator
 
 printf "Waiting for Operator to register custom resource definitions..."
 until oc get customresourcedefinitions servicemonitors.monitoring.coreos.com > /dev/null 2>&1; do sleep 1; printf "."; done
