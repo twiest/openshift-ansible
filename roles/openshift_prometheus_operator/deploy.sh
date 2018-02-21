b7cccb444f810abe04a27817aa7f3739179ef763
Elad Alfassa <ealfassa@redhat.com>
2018-02-21 17:01:58 +0200

    Don't add cluter-reader permission
    This is not needed anymore after the changes made in https://github.com/ironcladlou/openshift-ansible/commit/a2d33f6d40988a3949c6221cac3fdbc9309a4b64

--- roles/openshift_prometheus_operator/deploy.sh
+++ roles/openshift_prometheus_operator/deploy.sh
@@ -7,8 +7,6 @@ oc project monitoring
 
 oc apply -f files/manifests/prometheus-operator
 
-oc adm policy add-cluster-role-to-user cluster-reader -z prometheus-operator
-
 printf "Waiting for Operator to register custom resource definitions..."
 until oc get customresourcedefinitions servicemonitors.monitoring.coreos.com > /dev/null 2>&1; do sleep 1; printf "."; done
 echo "Done!"
